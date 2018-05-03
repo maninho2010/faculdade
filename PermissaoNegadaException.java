60bc3a0d6a1a05a0f41bd63954eace06f3a49f22
maninho2010 <32287210+maninho2010@users.noreply.github.com>
2018-05-03 18:17:31 -0300

    Delete PermissaoNegadaException.java

--- PermissaoNegadaException.java
+++
@@ -1,5 +0,0 @@
-public class PermissaoNegadaException extends RuntimeException {
-    public PermissaoNegadaException() {
-        super("Voce nao tem permissao para acessar esse conteudo");
-    }
-}
